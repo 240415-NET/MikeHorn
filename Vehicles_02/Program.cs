dd5c3647fac3e94ce362f19cfef3e045555af2fb
hornmb <hornmb1166@yahoo.com>
2024-04-24 14:37:16 -0400

    constuctors

--- Vehicles_02/Program.cs
+++ Vehicles_02/Program.cs
@@ -43,81 +43,94 @@ class Program
                             Vehicles.Add(new Vehicle());
 
                             Console.WriteLine("Please enter the Policy Id");
-                            Vehicles[Vehicles.Count() - 1].PolicyId = Convert.ToInt16(Console.ReadLine());
+                            Vehicles[Vehicles.Count() - 1].SetPolicyId(Convert.ToInt16(Console.ReadLine()));
 
                             Console.WriteLine("Please enter the Vehicle's Year");
-                            Vehicles[Vehicles.Count() - 1].year = Convert.ToInt16(Console.ReadLine());
+                            Vehicles[Vehicles.Count() - 1].Setyear(Convert.ToInt16(Console.ReadLine()));
 
                             Console.WriteLine("Please enter the Vehicle's Make");
-                            Vehicles[Vehicles.Count() - 1].make = Console.ReadLine();
+                            Vehicles[Vehicles.Count() - 1].Setmake(Console.ReadLine());
 
                             Console.WriteLine("Please enter the Vehicle's Model");
-                            Vehicles[Vehicles.Count() - 1].model = Console.ReadLine();
+                            Vehicles[Vehicles.Count() - 1].Setmodel(Console.ReadLine());
 
-                            Vehicles[Vehicles.Count() - 1].Active = true;
+                            Vehicles[Vehicles.Count() - 1].SetVehicleStatus(true);
+
+                            Vehicles[Vehicles.Count() - 1].SetVehicleNumber(Vehicles.Count());
 
                             break;
                         case 2: //toggle vehicle's active status
                             Console.WriteLine("Please enter the Vehicle's Indes Number to toggle its activate status");
                             int intIndex2;
+                            bool blnVehStatus;
+
                             intIndex2 = Convert.ToInt16(Console.ReadLine());
-                            Vehicles[intIndex2].Active = Vehicle.Toggle_Active(Vehicles[intIndex2].Active);
+
+                            blnVehStatus = Vehicles[intIndex2].GetVehicleStatus();
+
+                            Vehicles[intIndex2].SetVehicleStatus(Vehicle.Toggle_VehicleStatus(blnVehStatus));
+
                             break;
                         case 3: //list vehicles
-                            int intI1 = 0;
+                            // int intI1 = 0;
                             foreach(Vehicle v in Vehicles)
                             {
-                                string strActive = "Active";
-                                if(!v.Active)
-                                {
-                                    strActive = "Deactive";
-                                }
-                                Console.WriteLine($"Index = {intI1} Policy {v.PolicyId} Year {v.year} Make {v.make} Model {v.model} Active {strActive}");
-                                intI1++;
+                                Console.WriteLine(v);
+                                // string strActive = "Active";
+                                // if(!v.Active)
+                                // {
+                                //     strActive = "Deactive";
+                                // }
+                                // Console.WriteLine($"Index = {intI1} Policy {v.PolicyId} Year {v.year} Make {v.make} Model {v.model} Active {strActive}");
+                                // intI1++;
                             }
                             break;
                         case 4: //remove a vehicle
-                            Console.WriteLine("Please enter the Vehicle's Indes Number to remove");
+                            Console.WriteLine("Please enter the Vehicle's Index Number to remove");
                             Vehicles.RemoveAt(Convert.ToInt16(Console.ReadLine()));
                             break;
                         case 5: //enter bulk vehicles
                             //vehicle 0
-                            Vehicles.Add(new Vehicle() {PolicyId = 1234, year = 2018, make = "Ford", model = "F150", Active = true});
+                            // Vehicles.Add(new Vehicle() {PolicyId = 1234, year = 2018, make = "Ford", model = "F150", Active = true});
+                            Vehicles.Add(new Vehicle(1234, 2018, "Ford", "F150", true, Vehicles.Count()));
 
                             //vehicle 1
-                            Vehicles.Add(new Vehicle() {PolicyId = 1234, year = 2015, make = "Honda", model = "Accord", Active = true});
+                            // Vehicles.Add(new Vehicle() {PolicyId = 1234, year = 2015, make = "Honda", model = "Accord", Active = true});
+                            Vehicles.Add(new Vehicle(1234, 2015, "Honda", "Accord", true, Vehicles.Count()));
 
                             //vehicle 2
-                            Vehicles.Add(new Vehicle() {PolicyId = 1234, year = 2010, make = "Volkswagon", model = "Jetta", Active = true});
+                            // Vehicles.Add(new Vehicle() {PolicyId = 1234, year = 2010, make = "Volkswagon", model = "Jetta", Active = true});
+                            Vehicles.Add(new Vehicle(1234, 2010, "Volkswagon", "Jetta", true, Vehicles.Count()));
 
                             //vehicle 3
-                            Vehicles.Add(new Vehicle() {PolicyId = 5678, year = 2020, make = "Honda", model = "Civic", Active = true});
+                            // Vehicles.Add(new Vehicle() {PolicyId = 5678, year = 2020, make = "Honda", model = "Civic", Active = true});
+                            Vehicles.Add(new Vehicle(5678, 2020, "Honda", "Civic", true, Vehicles.Count()));
 
                             break;
                         case 6: //search
-                            int intIndex;
-                            string strMake;
-                            Console.WriteLine("What are you searching for?");
-                            strMake = Console.ReadLine().ToLower();
-                            intIndex = Vehicles.FindIndex(f => f.make.ToLower().Equals(strMake));
+                            // int intIndex;
+                            // string strMake;
+                            // Console.WriteLine("What are you searching for?");
+                            // strMake = Console.ReadLine().ToLower();
+                            // intIndex = Vehicles.FindIndex(f => f.make.ToLower().Equals(strMake));
 
-                            Console.Write($"The Make {strMake} is found at Index {intIndex} ");
+                            // Console.Write($"The Make {strMake} is found at Index {intIndex} ");
 
                             break;
                         case 7: //search with LINQ
                             
-                            Console.WriteLine("What are you searching for?");
-                            strMake = Console.ReadLine().ToLower();
+                            // Console.WriteLine("What are you searching for?");
+                            // strMake = Console.ReadLine().ToLower();
 
-                            // LINQ Query
-                            var subsetVehicles = from theVehicle in Vehicles
-                                        where theVehicle.make.ToLower() == strMake
-                                        select theVehicle;
-                            
-                            foreach(Vehicle theVehicle in subsetVehicles)
-                            {
-                                Console.WriteLine($"Policy {theVehicle.PolicyId} Year {theVehicle.year} Make {theVehicle.make} Model {theVehicle.model} Active {theVehicle.Active}");
-                            }
+                            // // LINQ Query
+                            // var subsetVehicles = from theVehicle in Vehicles
+                            //             where theVehicle.make.ToLower() == strMake
+                            //             select theVehicle;
+
+                            // foreach(Vehicle theVehicle in subsetVehicles)
+                            // {
+                            //     Console.WriteLine($"Policy {theVehicle.PolicyId} Year {theVehicle.year} Make {theVehicle.make} Model {theVehicle.model} Active {theVehicle.Active}");
+                            // }
 
                             break;
 
---
+++ Vehicles_02/Vehicle.cs
@@ -0,0 +1,124 @@
+namespace Vehicles_02;
+
+class Vehicle
+{
+    //Fields
+    private int PolicyId = 0;
+    private int year = 1900;
+    private string make = "";
+    private string model = "";
+    private bool VehicleStatus = true; //true = active, false = inactive
+    private int VehicleNumber = 1;
+
+    //Getters and Setters
+    //Policy Id
+    public int GetPolicyId()
+    {
+        return this.PolicyId;
+    }
+
+    public void SetPolicyId(int PolicyId)
+    {
+        this.PolicyId = PolicyId;
+    }
+
+    //year
+    public int Getyear()
+    {
+        return this.year;
+    }
+
+    public void Setyear(int year)
+    {
+        switch (year)
+        {
+            case <= 1900:
+                this.year = 1900;
+                break;
+            case > 2025:
+                this.year = 2025;
+                break;
+            default:
+                this.year = year;
+                break;
+        }
+        this.year = year;
+    }
+
+    //make
+    public string Getmake()
+    {
+        return this.make;
+    }
+
+    public void Setmake(string make)
+    {
+        this.make = make.Trim();
+    }
+
+    //model
+    public string Getmodel()
+    {
+        return this.model;
+    }
+
+    public void Setmodel(string model)
+    {
+        this.model = model.Trim();
+    }
+
+    //VehicleStatus
+    public bool GetVehicleStatus()
+    {
+        return this.VehicleStatus;
+    }
+
+    public void SetVehicleStatus(bool VehicleStatus)
+    {
+        this.VehicleStatus = VehicleStatus;
+    }
+
+    //VehicleNumber
+    public int GetVehicleNumber()
+    {
+        return this.VehicleNumber;
+    }
+
+    public void SetVehicleNumber(int VehicleNumber)
+    {
+        this.VehicleNumber = VehicleNumber;
+    }
+
+    //Constructors
+    public Vehicle(int PolicyId, int year, string make, string model, bool VehicleStatus, int VehicleNumber)
+    {
+        SetPolicyId(PolicyId);
+        Setyear(year);
+        Setmake(make);
+        Setmodel(model);
+        SetVehicleStatus(VehicleStatus);
+        SetVehicleNumber(VehicleNumber);
+
+    }
+
+    public Vehicle()
+    {
+
+    }
+
+    //Methods
+    public static bool Toggle_VehicleStatus(bool VehicleStatus)
+    {
+        return !VehicleStatus;
+    }
+
+    public override string ToString()
+    {
+        string VehicleStatusValue = "Active";
+        if(!VehicleStatus)
+        {
+            VehicleStatusValue = "Inactive";
+        }
+        return $"Index: {VehicleNumber} Policy: {PolicyId} Year: {year} Make: {make} Model: {model} Status: {VehicleStatusValue}";
+    }
+}
