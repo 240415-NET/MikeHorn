d8b12640805da09fa74997333144d18caf35ac8e
hornmb <hornmb1166@yahoo.com>
2024-05-14 08:33:35 -0400

    got first two fact tests working

--- Project1.Tests/Project1.Tests/Project1_Tests.cs
+++ Project1.Tests/Project1.Tests/Project1_Tests.cs
@@ -7,56 +7,63 @@ namespace Project1.Tests;
 
 public class Project1_Tests
 {
-    [Fact]
-    public void UserExists_False()
-    {
-        //Arrange
+    // [Fact]
+    // public void UserExists_False()
+    // {
+    //     //Arrange
         
 
-        //Act
-        bool testResult =  ProcessUserMenuItems.UserExists("aaa");
+    //     //Act
+    //     bool testResult =  ProcessUserMenuItems.UserExists("aaa");
 
-        //Assert
-        Assert.False(testResult);
-    }
+    //     //Assert
+    //     Assert.False(testResult);
+    // }
 
-    [Fact]
-    public void UserExists_True()
-    {
-        //Arrange
+    // [Fact]
+    // public void UserExists_True()
+    // {
+    //     //Arrange
         
 
-        //Act
-        bool testResult =  ProcessUserMenuItems.UserExists("OFlores");
+    //     //Act
+    //     bool testResult =  ProcessUserMenuItems.UserExists("OFlores");
 
-        //Assert
-        Assert.True(testResult);
-    }
+    //     //Assert
+    //     Assert.True(testResult);
+    // }
 
     [Fact]
     public void MainMenu_Agent_UserRole_False()
     {
         //Arrange
         MainMenuClass main = new MainMenuClass();
-        main.MainMenu("Agent");
+        main.MainMenu("Agent", true);
         string shouldHave = "manage users";
-        // bool testResult = true;
         
 
         //Act
-        // bool testResult =  main.MainMenu.strMainMenuItems.Contains("manage users");
         bool testResult = main.strMainMenuItems.Contains(shouldHave);
-        // if(main.strMainMenuItems.Count() == 3)
-        // {
-        //     testResult = true;
-        // }else
-        // {
-        //     testResult = false;
-        // }
-        
+
 
         //Assert
-        // Console.WriteLine(testResult);
         Assert.False(testResult);
+    }
+
+    [Fact]
+    public void MainMenu_Admin_UserRole_True()
+    {
+        //Arrange
+        MainMenuClass main = new MainMenuClass();
+        main.MainMenu("Admin", true);
+        string shouldHave = "manage users";
+        
+
+        //Act
+        bool testResult = main.strMainMenuItems.Contains(shouldHave);
+
+
+        //Assert
+        Assert.True(testResult);
     }
 }
